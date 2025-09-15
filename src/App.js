d8572a336f34d5f27c307bdc3c7a3f352c5b434e
moramahesh <maheswara215mora@gmail.com>
2025-09-15 12:57:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import './App.css';
+import NewModel from './components/shop';
+import LatestNews from './components/News';
+import Contact from './components/contact';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <NewModel />
+      <LatestNews/>
+      <Contact />
+      <Footer />
+    </Router>
   );
 }
 
